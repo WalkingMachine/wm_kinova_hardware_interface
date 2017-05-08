e83176a21fe1f38f921bb1c4d76b2d1497f15945
philippe117 <philippe117@live.fr>
2017-05-08 13:20:27 -0400

    added the KinovaHardwareInterface object

--- sara_arm/src/Lib/sara_arm_hardware_interface.h
+++ sara_arm/src/Lib/sara_arm_hardware_interface.h
@@ -8,7 +8,7 @@
 #include <hardware_interface/robot_hw.h>
 #include <string>
 
-const int NOMBRE_DE_MOTEURS_KINOVA = 5;
+int NOMBRE_DE_MOTEURS_KINOVA = 0;
 //Handle for the library's command layer.
 void * commandLayer_handle;
 int result;  // Tampon de réception des résultats de certaines fonctions
@@ -29,30 +29,37 @@ int (*MyGetAngularCommand)(AngularPosition &);
 int (*MyEraseAllTrajectories)();
 int (*MyGetSensorsInfo)(SensorsInfo &);
 int (*MySetActuatorMaxVelocity)(float &);
-int (*MyGetActuatorsPosition)(float *);
-int (*MyGetAngularVelocity)(float *);
+int (*MyGetActuatorsPosition)(float &);
+int (*MyGetAngularVelocity)(float &);
 
 
 
+std::vector<double> Offsets;
+std::vector<std::string> Names;
 
 
-class MyRobot : public hardware_interface::RobotHW {
+class ArmHardware : public hardware_interface::RobotHW {
     public:
-        MyRobot();
+        ArmHardware();
         void init();
         void Read();
         void Write();
-
+        //double Offset[8];
     private:
         hardware_interface::VelocityJointInterface joint_velocity_interface_;
         hardware_interface::JointStateInterface    joint_state_interface_;
         hardware_interface::JointStateHandle       joint_state_Handle_;
+
+        // Ros Control variables
         double cmd[8];
         double pos[8];
         double vel[8];
         double eff[8];
+
+        //Test variables
         double MaPos[8];
         double MaVel[8];
+        TrajectoryPoint pointToSend;
 
 };
 
