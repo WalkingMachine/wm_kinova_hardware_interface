3a39629a586035d0d887b176cc0cca0732e723c4
philippe117 <philippe117@live.fr>
2017-06-05 11:44:15 -0400

    ajout de sécutitées sur les gachettes et d'un estop

--- sara_arm/src/sara_simple_kinova_teleop.cpp
+++ sara_arm/src/sara_simple_kinova_teleop.cpp
@@ -7,16 +7,19 @@
 #include "../include/sara_arm/sara_arm_kinova_motors.h"
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
+#include <std_msgs/Bool.h>
 
 class ArmTeleop{
 public :
     ArmTeleop( );
 
 private:
+    bool EStop;
+    bool TeleopOn;
     ros::NodeHandle nh;
-    ros::Subscriber JoySub;
     kinova_hardware_interface* KHI[5];
     void JoyCB(const sensor_msgs::Joy::ConstPtr& joy );
+    void EStopCB(const std_msgs::Bool );
 };
 
 
@@ -28,35 +31,49 @@ ArmTeleop::ArmTeleop( ){
     KHI[3] = new arm_elbow_joint();
     KHI[4] = new arm_rot_wrist_joint();
 
+    TeleopOn = false;
+
     for ( int i=0; i< 5; i++ ){
         KHI[i]->init( nh );
     }
 
     nh = ros::NodeHandle();
 
-    // sub to joy
-    JoySub = nh.subscribe( "joy2", 1 , &ArmTeleop::JoyCB, this );
+    // sub
+    nh.subscribe( "joy2", 1 , &ArmTeleop::JoyCB, this );
+    nh.subscribe( "estop", 1 , &ArmTeleop::EStopCB, this );
 
 }
 
 
 void ArmTeleop::JoyCB(const sensor_msgs::Joy::ConstPtr& joy ){
 
+    if (TeleopOn && !EStop ) {
+        for ( int i=0; i< 5; i++ ){
+            KHI[i]->Read();
+        }
+        KHI[0]->cmd = joy->axes[0]*-15;
+        KHI[1]->cmd = joy->axes[1]*-25;
+        KHI[2]->cmd = joy->axes[3]* 30;
+        KHI[3]->cmd = joy->axes[4]*-30;
+        KHI[4]->cmd = ((joy->axes[2]) - (joy->axes[5])) * -30;
 
-    for ( int i=0; i< 5; i++ ){
-        KHI[i]->Read();
+        for ( int i=0; i< 5; i++ ){
+            KHI[i]->Write();
+        }
+    } else if ( joy->axes[2]  <= -0.999 && joy->axes[5] <= -0.999 ){
+        TeleopOn = true;
+        EStop = false;
     }
 
-    KHI[0]->cmd = joy->axes[0]*-15;
-    KHI[1]->cmd = joy->axes[1]*-25;
-    KHI[2]->cmd = joy->axes[3]* 30;
-    KHI[3]->cmd = joy->axes[4]*-30;
-    KHI[4]->cmd = ((joy->axes[2])-(joy->axes[5]))*30;
-
-    for ( int i=0; i< 5; i++ ){
-        KHI[i]->Write();
-    }
 }
+
+void ArmTeleop::EStopCB(const std_msgs::Bool msg ){
+    ROS_INFO( "estop: %c", msg.data  );
+    EStop = ( msg.data == '1' );
+}
+
+
 
 
 int main(int argc, char **argv) {
